382d18e3367d8826b2472d6c7e58ede6b97926b1
Karthik samy <karthik.samy@a-cti.com>
2018-08-02 14:21:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 
 DOWNLOAD_URL=https://github.com/karthikAdaptavant/XcodeWay/blob/master/XcodeWayExtensions/Script/XcodeWayScript.scpt
 SCRIPT_DIR="${HOME}/Library/Application Scripts/com.fantageek.XcodeWayApp.XcodeWayExtensions"
-
 mkdir -p "${SCRIPT_DIR}"
 curl $DOWNLOAD_URL -o "${SCRIPT_DIR}/XcodeWayScript.scpt"
